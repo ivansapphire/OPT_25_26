e4fddfd0d5ae2a88fb30ec9e9b7c4fb6b24e1ac7
iván <ivanspyro07@gmail.com>
2025-10-06 13:38:53 +0200

    ej4.2

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -8,14 +8,14 @@ print(f"Para dividir pulsa 4")
 
 opt = int(input("Dime la opción  que quieres"))
 
-
-if (opt ==1):
-    print(f"La suma de tus números es {num1 + num2}!")
-elif (opt ==2):
-    print(f"La resta de tus números es {num1 - num2}!")
-elif (opt ==3):
-    print(f"La multiplicación de tus números es {num1 * num2}!")
-elif (opt ==4):
-    print(f"La división de tus números es {num1 / num2}!")
-
-else: print("Opción no válida, el número no es ninguna de las opciones posibles")
+match opt:
+    case  1:
+        print(f"La suma de tus números es {num1 + num2}!")
+    case 2:
+        print(f"La resta de tus números es {num1 - num2}!")
+    case  3:
+        print(f"La multiplicación de tus números es {num1 * num2}!")
+    case 4:
+        print(f"La división de tus números es {num1 / num2}!")
+    case _:
+        print("Opción no válida, el número no es ninguna de las opciones posibles")
